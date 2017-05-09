bc47bd2b9c5aab618ddfb50116e58c78e0208c7e
rishikeshwar <rishikeshwar1@gmail.com>
2017-05-09 20:00:43 +0530

    bug

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 
 module Scaff
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+config.assets.initialize_on_precompile = false
   end
 end
