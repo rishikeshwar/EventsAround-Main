7a1a79d387f45102b10b4f18314398b201fdde61
rishikeshwar <rishikeshwar1@gmail.com>
2017-05-27 14:58:07 +0530

    bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
+  config.public_file_server.enabled = true
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
